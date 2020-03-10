0965e262bc3ffbaf3aa18a0877a6dbdeb7fb4172
Gaurav Aggarwal <aggarwal.gaurav10@gmail.com>
2020-03-10 18:49:52 +0530

    Adding book successfully

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,26 +1,42 @@
 import React, { useState } from 'react';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 
-import { addBookQuery, getAuthorsQuery } from '../queries/queries';
+import { addBookQuery, getAuthorsQuery, getBooksQuery } from '../queries/queries';
 
 export default function AddBook() {
     const [name, setName] = useState('');
     const [genre, setGenre] = useState('');
-    const [author, setAuthor] = useState('');
+    const [authorId, setAuthorId] = useState('');
 
     let { loading, error, data } = useQuery(getAuthorsQuery);
-    let [saveBook, { loading1 }] = useMutation(addBookQuery);
+    let [saveBook] = useMutation(addBookQuery);
 
-    let authors;
+    let authors = <option>Loading Authors...</option>;
 
-	if (loading) authors = <option>Loading Authors...</option>;
-    else authors = data.authors.map(author => <option key={author.id} value={author.id}>{author.name}</option>)
+	if (!loading && data) {
+        authors = data.authors.map(author => <option key={author.id} value={author.id}>{author.name}</option>);
+    }
 
     if (error) return <p>Error </p>;
     
-    function submitForm(e, state) {
+    function submitForm(e) {
         e.preventDefault();
-        // saveBook();
+        
+        // Run mutation that will save the book
+        saveBook({
+            variables: {
+                name, 
+                genre, 
+                authorId 
+            },
+            refetchQueries: [{ query: getBooksQuery}]
+        });
+        
+        // Resetting the form
+        setName('');
+        setGenre('');
+        setAuthorId('');
+        console.log('Saved');
     }
 
 	return (
@@ -28,15 +44,15 @@ export default function AddBook() {
 			<form id='add-book' onSubmit={submitForm}>
                 <div className='filed'>
                     <label>Book Name:</label>
-                    <input type='text' onChange={e => setName(e.target.value)}/>
+                    <input type='text' value={name} onChange={e => setName(e.target.value)}/>
                 </div>
                 <div className='filed'>
                     <label>Genre:</label>
-                    <input type='text' onChange={e => setGenre(e.target.value)}/>
+                    <input type='text' value={genre} onChange={e => setGenre(e.target.value)}/>
                 </div>
                 <div className='filed'>
                     <label>Author:</label>
-                    <select onChange={e => setAuthor(e.target.value)}>
+                    <select value={authorId} onChange={e => setAuthorId(e.target.value)}>
                         <option>Select Author</option>
                         {authors}
                     </select>
